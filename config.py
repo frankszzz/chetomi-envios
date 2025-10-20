26b504e26f6c2d29b05bbeeb37ac8229a88b1bf8
frankszzz <141263434+frankszzz@users.noreply.github.com>
2025-10-19 23:14:32 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'defaultsecret')
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
-        f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT', 5432)}/{os.getenv('POSTGRES_DB')}"
-    )
+    SECRET_KEY = 'tu_secret_key'  # Cambia por uno seguro
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OPENROUTESERVICE_API_KEY = os.getenv('OPENROUTESERVICE_API_KEY', '')
